9c7f44c2c4461c52dee7953b748a7df30b994eb0
Liam Mulhern <liam.mulhern@outlook.com>
2023-10-05 21:35:13 +1000

    Adds initial assignment structure

---
+++ structures/m_stack.py
@@ -0,0 +1,50 @@
+"""
+Sample Solution: COMP3506/7505 A1, S2, 2023
+The University of Queensland
+
+This file WILL NOT be used for marking in A2, but if you do use/modify it
+you should submit your modified version.
+"""
+from typing import Any
+
+from structures.m_extensible_list import ExtensibleList
+
+
+class Stack:
+    """
+    A simple composition-based stack backed by an ExtensibleList
+    """
+
+    def __init__(self) -> None:
+        self._data = ExtensibleList()
+
+    def push(self, elem: Any) -> None:
+        """
+        Push some data `elem` onto the stack
+        """
+        self._data.append(elem)
+
+    def pop(self) -> Any | None:
+        """
+        Remove and return the top element or None if empty
+        """
+        if self._data.is_empty():
+            return None
+        index = self._data.get_size() - 1
+        value = self._data.remove_at(index)
+        return value
+
+    def peek(self) -> Any | None:
+        """
+        Return the top element but do not remove it
+        """
+        index = self._data.get_size() - 1
+        # get_at handles indexes being out of bounds
+        return self._data.get_at(index)
+
+    def is_empty(self) -> bool:
+        return self._data.is_empty()
+
+    def get_size(self) -> int:
+        return self._data.get_size()
+
---
+++ structures/m_util.py
@@ -0,0 +1,47 @@
+"""
+COMP3506/7505 S2 2023
+The University of Queensland
+
+NOTE: This file will be used for marking.
+"""
+
+from enum import Enum
+from abc import ABC, abstractmethod 
+
+class Hashable(ABC):
+    """
+    A special object that can be inherited to enforce objects to be hashable
+    """
+    
+    def __init__(self) -> None:
+        """
+        You are free to do anything you find suitable to initialise your
+        Hashable class. But maybe you don't need to do anything!
+        """
+        pass
+
+    @abstractmethod
+    def get_hash(self) -> int:
+        """
+        Return an integer hash of the given object. You MUST use this
+        if you wish to hash keys of a specific type. See m_entry.py for more
+        help in this direction, as well as m_map.py
+        """
+        pass
+
+
+class TraversalFailure(Enum):
+    """
+    Enums to handle special graph problems we might want to flag.
+    """
+    DISCONNECTED = 1
+    """ The graph is disconnected and the origin and the target lie in different components. """
+
+    NEGATIVE_CYCLE = 2
+    """ The graph contains a cycle of negative weight 
+    and the path between the origin and the target passes through it. """
+
+
+"""
+Any other utilities can go below here.
+"""
